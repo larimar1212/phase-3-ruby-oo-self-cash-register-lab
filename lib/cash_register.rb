2b8b01d61b50245acfbe1f2e1aac1e83e1ddce34
larimar <larimarfondeur@gmail.com>
2022-08-31 18:30:03 -0400

    commit tree f18f8c9cb4be9f81dda8cdc679eeadd0a6c11d6e

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+class CashRegister
 
+  attr_accessor :items, :discount, :total, :last_transaction
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, amount, quantity = 1)
+    self.last_transaction = amount * quantity
+    self.total += self.last_transaction
+    quantity.times do
+      self.items << title
+    end
+  end
+
+ 
+end
